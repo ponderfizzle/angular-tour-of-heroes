b59874b206755c3ba4e0fe974ededbd6e7a586d1
ponderfizzle <derek.bodi@gmail.com>
2021-04-15 17:30:28 -0400

    hero editor completed

---
+++ src/app/interfaces/Hero.ts
@@ -0,0 +1,7 @@
+export interface Hero {
+  // I don't like not prefacing an interface with an I but apparently that's wrong in Typescript?
+  // The tutorial states to do it this way so that is what is implemented.
+
+  id: number;
+  name: string;
+}
